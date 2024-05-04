488f8c52dd59158326ad25a42c1780ea9dc92f6e
Precious <Preshomondi@gmail.com>
2024-05-04 22:45:54 +0300

    Create jd post functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, {useState} from 'react';
+import Job from './components/Job';
+
 import Doc from './components/Doc';
 import './App.css';
 
 function App() {
-  const handleDocumentUpload = () => {
-    console.log("CV Uploaded")
+const [uploadedDoc, setUploadedDoc] = useState(null)
+
+  const handleDocumentUpload = (document) => {
+    setUploadedDoc(document)
+  }
+
+  const handleJobSubmit = (jd) => {
+    console.log('Job description submitted', jd)
+
   }
   
   return (
     <div className="App">
       <Doc handleUpload={handleDocumentUpload}/>
+      <Job onSubmit={handleJobSubmit}/>
+      
       
     </div>
   );
--- src/components/Job.js
+++ src/components/Job.js
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Job() {
+function Job({onSubmit}) {
+  const [jd, setJd] = useState('')
+  const [isDisabled, setIsDisabled] = useState(true)
+
+const handleTextareaChange = (e) => {
+  const text = e.target.value
+  setJd(text)
+  setIsDisabled(text.trim().length === 0)
+}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit(jd)
+    setJd('')
+  }
+
   return (
-    <div>Job</div>
+   <form onSubmit={handleSubmit}>
+     <textarea
+     value={jd}
+     onChange={handleTextareaChange}
+     placeholder="Enter job description here..."
+     
+     />
+     <button type='submit' disabled={isDisabled}>Post Job</button>
+   </form>
   )
 }
 
