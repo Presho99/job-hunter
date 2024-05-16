3ad761c1377fc0b39b9ac34a4bb13ab248a12844
Precious <Preshomondi@gmail.com>
2024-05-16 05:35:25 +0300

    Create the layout

--- src/App.js
+++ src/App.js
@@ -1,39 +1,41 @@
-import React, {useState} from 'react';
-import Job from './components/Job';
-import Keywords from './components/Keywords';
-import Doc from './components/Doc';
-import Score from './components/Score'
-import Correction from './components/Correction';
-import './App.css';
+import React, { useState } from "react";
+import Job from "./components/Job";
+import Keywords from "./components/Keywords";
+import Doc from "./components/Doc";
+import Score from "./components/Score";
+import Correction from "./components/Correction";
+import "./App.css";
 
 function App() {
-const [uploadedDoc, setUploadedDoc] = useState(null)
-const [jobDescription, setJobDescription] = useState('')
+  const [uploadedDoc, setUploadedDoc] = useState(null);
+  const [jobDescription, setJobDescription] = useState("");
 
   const handleDocumentUpload = (document) => {
-    setUploadedDoc(document)
-  }
+    setUploadedDoc(document);
+  };
 
   const handleJobSubmit = (jd) => {
-    console.log('Job description submitted', jd)
-    setJobDescription(jd)
+    console.log("Job description submitted", jd);
+    setJobDescription(jd);
+  };
 
-  }
-  
   return (
     <div className="App">
-      <Doc handleUpload={handleDocumentUpload}/>
-      <Job onSubmit={handleJobSubmit}/>
-      <Keywords jobDescription={jobDescription}/>
-      {uploadedDoc && jobDescription && (
-        <div>
-          <Score jobDescription={jobDescription} uploadedDoc={uploadedDoc}/>
-        <Correction jobDescription={jobDescription} uploadedDoc={uploadedDoc}/>
+      <div className="grid-container">
+        <div className="left">
+          <Score jobDescription={jobDescription} uploadedDoc={uploadedDoc} />
+          <Keywords jobDescription={jobDescription} />
         </div>
-        
-      )}
-      
-      
+        <div className="middle">
+          <Doc handleUpload={handleDocumentUpload} />
+          <Job onSubmit={handleJobSubmit} />
+        </div>
+        <div className="right">
+        <Correction jobDescription={jobDescription} uploadedDoc={uploadedDoc} />
+        </div>
+
+     
+      </div>
     </div>
   );
 }
