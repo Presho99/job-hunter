996cf79564218eccf0e9f9344859c1ec69a3b562
Precious <Preshomondi@gmail.com>
2024-05-21 17:02:00 +0300

    Interview prep questions generation

--- src/components/Prep.js
+++ src/components/Prep.js
@@ -1,11 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import '../components/Prep.css';
 
 function Prep({ uploadedDoc, jobDescription }) {
   const genAI = new GoogleGenerativeAI("AIzaSyD94CdKXLoxcR_u-hb7Y1TR9cHHQ2aMT1c");
-  const [suggestions, setSuggestions] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [dsaQuest, setDsaQuest] = useState(null);
+  const [techQuest, setTechQuest] = useState(null);
+  const [behaviorQuest, setBehaviorQuest] = useState(null);
+  const [currentQuest, setCurrentQuest] = useState(null);
 
   const generateQuestions = async (type) => {
     if (!jobDescription) {
@@ -17,15 +20,15 @@ function Prep({ uploadedDoc, jobDescription }) {
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
     let prompt;
 
-    switch(type) {
+    switch (type) {
       case 'dsa':
-        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate possible data structure and algorithm questions. Don't list the qualifications. Just the questions.`;
+        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate 5 possible interview questions on data structures and algorithms. Don't list the qualifications. Just the questions only.`;
         break;
       case 'behavioral':
-        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate possible behavioral interview questions. Don't list the qualifications. Just the questions.`;
+        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate 5 possible behavioral interview questions. Don't list the qualifications. Just the questions only.`;
         break;
       case 'technical':
-        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate possible technical interview questions related to the specific technologies and skills mentioned. Don't list the qualifications. Just the questions.`;
+        prompt = `The job description is as follows: ${jobDescription}. The CV provided is ${uploadedDoc}. Please use the job description to generate 5 possible technical interview questions related to the specific technologies and skills mentioned. Don't list the qualifications. Just the questions only.`;
         break;
       default:
         console.error("Invalid question type");
@@ -37,7 +40,24 @@ function Prep({ uploadedDoc, jobDescription }) {
       const result = await model.generateContent(prompt);
       const response = await result.response;
       const questions = await response.text();
-      setSuggestions(questions);
+
+      switch (type) {
+        case 'dsa':
+          setDsaQuest(questions);
+          setCurrentQuest(questions);
+          break;
+        case 'behavioral':
+          setBehaviorQuest(questions);
+          setCurrentQuest(questions);
+          break;
+        case 'technical':
+          setTechQuest(questions);
+          setCurrentQuest(questions);
+          break;
+        default:
+          break;
+      }
+
     } catch (error) {
       console.error("Error generating content:", error);
     } finally {
@@ -45,31 +65,49 @@ function Prep({ uploadedDoc, jobDescription }) {
     }
   };
 
+  useEffect(() => {
+    if (jobDescription) {
+      generateQuestions('dsa');
+    }
+  }, [jobDescription]);
+
   return (
     <div className="prep">
-
       <div className="prep-buttons">
-            <button onClick={() => generateQuestions('dsa')}>DSAs</button>
-            <button onClick={() => generateQuestions('technical')}>Technical</button>
-            <button onClick={() => generateQuestions('behavioral')}>Behavioral</button>
-          </div>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="prep-box">
-             
-          {suggestions ? (
+        <button onClick={() => setCurrentQuest(dsaQuest)}>DSAs</button>
+        <button onClick={() => {
+          if (!techQuest) generateQuestions('technical');
+          else setCurrentQuest(techQuest);
+        }}>Technical</button>
+        <button onClick={() => {
+          if (!behaviorQuest) generateQuestions('behavioral');
+          else setCurrentQuest(behaviorQuest);
+        }}>Behavioral</button>
+      </div>
+
+      <div className="prep-box">
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          currentQuest ? (
             <ul>
-              {suggestions.split("\n").map((suggestion, index) => (
+              {currentQuest.split("\n").map((question, index) => (
                 <li key={index} className="prep-key">
-                  {suggestion.replace(/^\*\s*/, "")}
+                  {question.replace(/^\*\s*/, "")}
                 </li>
               ))}
             </ul>
-          ) : null}
-         
+          ) : null
+        )}
+        <div className="prep-below">
+        <p>Practice questions will appear here</p>
         </div>
-      )}
+       
+      </div>
+
+      <div className="prep-footer">
+        <p>Powered by Gemini</p>
+      </div>
     </div>
   );
 }
